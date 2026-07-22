3b529941975ad705107de70de465c68c4159c2a0
Sara Caviglia <saracaviglia@hotmail.com>
2026-07-22 11:13:10 +0200

    updated reports for icx, added n15000

---
+++ _project/WK008/n15000/matrixmul.c
@@ -0,0 +1,60 @@
+#define n 15000
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+    int i, j, k;
+    time_t start, end;
+
+    start = time(NULL);
+
+    double (*a)[n] = malloc(sizeof(double[n][n]));
+    double (*b)[n] = malloc(sizeof(double[n][n]));
+    double (*c)[n] = malloc(sizeof(double[n][n]));
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+        {
+            a[i][j] = 2.0;
+            b[i][j] = 3.0;
+            c[i][j] = 0.0;
+        }
+
+    for (i = 0; i < n; ++i)
+        for (k = 0; k < n; k++)
+            for (j = 0; j < n; ++j)
+                c[i][j] += a[i][k] * b[k][j];
+
+    FILE *f = fopen("mat-res.txt", "w");
+    if (!f)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    fprintf(f, "%d\n\n", n);
+    for (int i = 0; i < 1000; i++)
+    {
+        for (int j = 0; j < 1000; j++)
+        {
+            fprintf(f, "%.0f ", c[i][j]);
+        }
+        fprintf(f, "\n");
+    }
+
+    fclose(f);
+
+    free(a);
+    free(b);
+    free(c);
+
+    end = time(NULL);
+
+    printf("Time taken to multiply the matrices: %.2lf seconds.\n", difftime(end, start));
+
+    return 0;
+}
